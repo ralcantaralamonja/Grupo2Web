b4e5523423c3b3a822a1badf9d419d2dbd41f914
Ricardo Alcantara Lamonja <alcantararicardo04@gmail.com>
2022-10-03 21:41:25 -0500

    Parte 5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
-    
-
-
-  
-        
-        
+  $("#boton").mouseenter(function () {
+    $("#boton").css("background-color", "#FFFFFF").css("color", "#000000");
+  });
+  $("#boton").mouseleave(function () {
+    $("#boton").css("background-color", "#b60472").css("color", "#FFFFFF");
+  });
 });
